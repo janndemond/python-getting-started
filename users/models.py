104a2e7fd4bc6d1729865e9e549da34ce561a164
janndemond <64373823+janndemond@users.noreply.github.com>
2020-06-03 15:45:02 +0200

    addProfile

--- users/models.py
+++ users/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 class profile (models.Model):
-    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    user = models.OneToOneField(User,on_delete=models.CASCADE,default="test")
     image = models.ImageField(default='defould.jpg',upload_to='profile_pics')
 
     def __str__(self):
--- users/signals.py
+++ users/signals.py
@@ -1,4 +1,4 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, post_init
 
 from django.contrib.auth.models import User
 from   django.dispatch import receiver
@@ -8,7 +8,10 @@ def create_profile(sender,instance, created,**kwargs ):
     if created:
         profile.objects.create(user=instance)
 
-
+@receiver(post_init,sender=User)
+def create_profile(sender,instance, created,**kwargs ):
+    if created:
+        profile.objects.create(user=instance)
 @receiver(post_save,sender=User)
 def seva_profile(sender,instance,**kwargs):
     instance.profile.save()
